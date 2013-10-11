ad83595dffd66e512cb37f3a8e577713cd45a9ef
daeks <daeks@gmx.net>
2013-10-11 22:02:07 +0200

    add version check

---
+++ dialog.php
@@ -0,0 +1,11 @@
+<!--
+    Copyright (c) Codiad, distributed
+    as-is and without warranty under the MIT License. 
+    See [root]/license.md for more information. This information must remain intact.
+-->
+<form id="dateForm">
+    <label>Codiad Collaborative</label>
+    <pre>Not compatible with this version of Codiad</pre>
+    <em class="note">Note: This plugin requires at least Codiad 2.x.</em><br><br>
+    <button onclick="codiad.modal.unload(); return false;">Close</button>
+</form>
